aee4a80ee498f8f2b4017d025e02d316494e6e1c
philippschmalen <philippschmalen@gmail.com>
2021-04-22 19:41:38 +0200

    changed folder structure for streamlit_app sharing, added overall trend last 30 days

---
+++ run_streamlit_app.bat
@@ -0,0 +1,23 @@
+@echo OFF
+rem How to run a Python script in a given conda environment from a batch file.
+
+rem It doesn't require:
+rem - conda to be in the PATH
+rem - cmd.exe to be initialized with conda init
+
+rem Define here the path to your conda installation
+set CONDAPATH=C:\Users\phili\miniconda3
+rem Define here the name of the environment
+set ENVNAME=sai
+
+rem The following command activates the base environment.
+rem call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
+if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=%CONDAPATH%\envs\%ENVNAME%)
+
+rem Activate the conda environment
+call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
+
+rem Run a python script in that environment
+streamlit run streamlit_app.py
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from utils.app_utilities import load_config, load_data
+from streamlit_utils.app_utilities import load_config, load_data
 from datetime import datetime, tzinfo, date
 
 
@@ -23,7 +23,6 @@ df['date'] = pd.to_datetime(df['date']).dt.tz_localize(None)
 
 
 # -- choice by user
-
 # choose platform
 platform_options = df.platform.unique().tolist()
 platform_selected = st.sidebar.multiselect("Select platform(s)", 
@@ -31,8 +30,7 @@ platform_selected = st.sidebar.multiselect("Select platform(s)",
 							default=platform_options)
 # choose time window
 date_range = st.sidebar.date_input("Date range (default: last 6 months)", 
-	[df['date'].max() - pd.to_timedelta(180, unit='d'), 
-	df['date'].max()])
+	[df['date'].max() - pd.to_timedelta(180, unit='d'), df['date'].max()])
 # choose date detail level
 date_unit_options = {'D': "daily", 'W': "weekly", 'M': "monthly"}
 detail_level = st.sidebar.selectbox("Detail level", 	options=list(date_unit_options.keys()), 
@@ -54,10 +52,29 @@ df_plot = (
 		.reset_index()
 		.sort_values('date'))
 
+# compare last 30 days with 30 days before
+t_l30d = df.loc[df['date'].between(
+	(df['date'].max() - pd.to_timedelta(30, unit='d')), 
+	df['date'].max()) \
+	& df.platform.isin(platform_selected)].value.sum()
+t_l60to30d = df.loc[df['date'].between(
+	(df['date'].max() - pd.to_timedelta(30*2, unit='d')), 
+	(df['date'].max() - pd.to_timedelta(30, unit='d'))) \
+	& df.platform.isin(platform_selected)].value.sum()
+absolute_change = t_l30d - t_l60to30d
+relative_change = round(((t_l30d/t_l60to30d)-1)*100, 1)
+
+f"""## Trend last 30 days*: `{relative_change}`%
+
+**Total views** ast 30 days: `{t_l30d}` 
+
+compared to previous 30 days: `{t_l60to30d}`
+
+	*{*platform_selected,}
+""" 
 
 
-
-# -- overall figure
+# -- total sum over select time period
 st.write(f"""
 		### Overall {df_plot.value.sum()} visits within the last {(date_range[-1]-date_range[0]).days} days.
 	""")
@@ -65,8 +82,7 @@ st.write(f"""
 
 
 
-
-# -- Line plot across time
+# -- Plot across time
 fig = px.line(df_plot, x='date', y='value', color='platform', 
 	labels={
 		'date_grouping': "Date", 
@@ -87,6 +103,5 @@ fig_tree = px.treemap(df_treemap, path=['platform'], values='share')
 st.plotly_chart(fig_tree)
 
 
-
-
-
+'## Inspect data for interaction metric'
+'### Twitter raw data', pd.read_csv("./data/raw/daily_tweet_activity_metrics_InitiativeSuAv_20210325_20210422_en.csv")
